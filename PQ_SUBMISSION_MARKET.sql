4d4703f2e2c614bd06234099cd28d988bb4dd5d6
eg0nzales <egonzales@nrchealth.com>
2025-04-21 08:54:00 -0400

    Create PQ_SUBMISSION_MARKET.sql

---
+++ PQ_SUBMISSION_MARKET.sql
@@ -0,0 +1,54 @@
+WITH AllMarkets AS (
+    SELECT 
+        NULL AS Client_id,
+        NULL AS client_nm,
+        market_id,
+        market_dsc
+    FROM [HCMG_Dev_FromProd].[dbo].Market
+
+    UNION
+
+    SELECT 
+        NULL AS Client_id,
+        NULL AS client_nm,
+        market_id,
+        market_dsc
+    FROM dbadata.dbo.vulcan_market
+),
+DedupedMarkets AS (
+    SELECT 
+        market_id, 
+        market_dsc,
+        ROW_NUMBER() OVER (PARTITION BY market_id ORDER BY market_dsc) AS rn
+    FROM AllMarkets
+)
+SELECT 
+    c.MI10ClientId AS Client_id,
+    c.customername AS client_nm,
+    m.market_id AS Market_id,
+    m.market_dsc AS market_dsc
+FROM dbadata.dbo.pimisql02_customers c
+JOIN dbadata.dbo.pimisql02_brands b ON c.MI10ClientId = b.customerid
+JOIN dbadata.dbo.pimisql02_brandmarkets bm ON b.brandid = bm.brandid
+JOIN dbadata.dbo.vulcan_market m ON bm.marketid = m.market_id
+
+UNION
+
+SELECT 
+    mq.Client_id,
+    mq.client_nm,
+    mq.Market_id,
+    mq.market_dsc
+FROM [HCMG_Dev_FromProd].[PQ].MarketQuestion_view mq
+
+UNION
+
+SELECT 
+    NULL AS Client_id,
+    NULL AS client_nm,
+    market_id,
+    market_dsc
+FROM DedupedMarkets
+WHERE rn = 1
+
+ORDER BY Client_nm, Market_dsc;
