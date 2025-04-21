47501c58a935a7df5ff8196b362ccf7005ad89b1
eg0nzales <egonzales@nrchealth.com>
2025-04-21 09:38:36 -0400

    Update PQ_SUBMISSION_MARKET.sql

--- PQ_SUBMISSION_MARKET.sql
+++ PQ_SUBMISSION_MARKET.sql
@@ -21,34 +21,44 @@ DedupedMarkets AS (
         market_dsc,
         ROW_NUMBER() OVER (PARTITION BY market_id ORDER BY market_dsc) AS rn
     FROM AllMarkets
+),
+ClientData AS (
+    SELECT 
+        c.MI10ClientId AS Client_id,
+        c.customername AS client_nm,
+        m.market_id AS Market_id,
+        m.market_dsc AS market_dsc
+    FROM dbadata.dbo.pimisql02_customers c
+    JOIN dbadata.dbo.pimisql02_brands b ON c.MI10ClientId = b.customerid
+    JOIN dbadata.dbo.pimisql02_brandmarkets bm ON b.brandid = bm.brandid
+    JOIN dbadata.dbo.vulcan_market m ON bm.marketid = m.market_id
+
+    UNION
+
+    SELECT 
+        mq.Client_id,
+        mq.client_nm,
+        mq.Market_id,
+        mq.market_dsc
+    FROM [HCMG_Dev_FromProd].[PQ].MarketQuestion_view mq
+
+    UNION
+
+    SELECT 
+        NULL AS Client_id,
+        NULL AS client_nm,
+        market_id,
+        market_dsc
+    FROM DedupedMarkets
+    WHERE rn = 1
 )
+-- Final Selection
 SELECT 
-    c.MI10ClientId AS Client_id,
-    c.customername AS client_nm,
-    m.market_id AS Market_id,
-    m.market_dsc AS market_dsc
-FROM dbadata.dbo.pimisql02_customers c
-JOIN dbadata.dbo.pimisql02_brands b ON c.MI10ClientId = b.customerid
-JOIN dbadata.dbo.pimisql02_brandmarkets bm ON b.brandid = bm.brandid
-JOIN dbadata.dbo.vulcan_market m ON bm.marketid = m.market_id
-
-UNION
-
-SELECT 
-    mq.Client_id,
-    mq.client_nm,
-    mq.Market_id,
-    mq.market_dsc
-FROM [HCMG_Dev_FromProd].[PQ].MarketQuestion_view mq
-
-UNION
-
-SELECT 
-    NULL AS Client_id,
-    NULL AS client_nm,
-    market_id,
-    market_dsc
-FROM DedupedMarkets
-WHERE rn = 1
-
-ORDER BY Client_nm, Market_dsc;
+    cd.Client_id,
+    cd.client_nm,
+    cd.Market_id,
+    cd.market_dsc
+FROM ClientData cd
+LEFT JOIN [HCMG_Dev_FromProd].[dbo].Client cl ON cd.Client_id = cl.Client_id
+WHERE (cl.datRemoved IS NULL OR cl.datRemoved >= CAST(GETDATE() AS DATETIME) OR cd.Client_id IS NULL)
+ORDER BY cd.client_nm, cd.market_dsc;
