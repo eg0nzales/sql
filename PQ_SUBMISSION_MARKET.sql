bba648919e8ac894729723552c2a0985147e5cd0
eg0nzales <egonzales@nrchealth.com>
2025-07-30 22:01:44 -0400

    Update PQ_SUBMISSION_MARKET.sql

--- PQ_SUBMISSION_MARKET.sql
+++ PQ_SUBMISSION_MARKET.sql
@@ -25,11 +25,11 @@ DedupedMarkets AS (
 ),
 
 ClientData AS (
-    -- From Brand-to-Market relationships with proper joins and columns
+    -- From Brand-to-Market relationships
     SELECT 
-        c.CustomerId AS Client_id,
+        c.MI10ClientId AS Client_id,
         c.customername AS client_nm,
-        bm.MarketId AS market_id,
+        bm.marketid AS market_id,
         vm.market_dsc AS market_dsc
     FROM dbadata.dbo.pimisql02_customers c
     INNER JOIN dbadata.dbo.pimisql02_brands b 
@@ -37,9 +37,9 @@ ClientData AS (
     INNER JOIN dbadata.dbo.pimisql02_brandmarkets bm 
         ON b.BrandId = bm.BrandId
     INNER JOIN dbadata.dbo.vulcan_market vm 
-        ON bm.MarketId = vm.market_id
+        ON bm.marketid = vm.market_id
 
-    UNION
+    UNION ALL
 
     -- From MarketQuestion_view (PQ assignments)
     SELECT 
@@ -49,7 +49,7 @@ ClientData AS (
         mq.market_dsc
     FROM [HCMG_Dev_FromProd].[PQ].MarketQuestion_view mq
 
-    UNION
+    UNION ALL
 
     -- Unassigned deduplicated markets
     SELECT 
@@ -71,13 +71,13 @@ LEFT JOIN [HCMG_Dev_FromProd].[dbo].Client cl
     ON cd.Client_id = cl.Client_id
 WHERE 
     (cl.datRemoved IS NULL OR cl.datRemoved >= CAST(GETDATE() AS DATETIME) OR cd.Client_id IS NULL)
-    AND COALESCE(cd.market_dsc, '') NOT LIKE 'xx%' 
-    AND COALESCE(cd.market_dsc, '') NOT LIKE 'zz%' 
+    AND cd.market_dsc NOT LIKE 'xx%' 
+    AND cd.market_dsc NOT LIKE 'zz%' 
     AND NOT (
         cd.client_nm IS NULL 
         AND cd.market_dsc NOT LIKE '%-%'
     )
 ORDER BY 
-    CASE WHEN cd.client_nm IS NULL THEN 1 ELSE 0 END,  -- unassigned last
+    CASE WHEN cd.client_nm IS NULL THEN 1 ELSE 0 END, 
     cd.client_nm,
     cd.market_dsc;
